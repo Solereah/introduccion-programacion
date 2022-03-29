c82d9a80ac3dbee874567f5eb25dc33084fabf68
Sol <slereah@gmail.com>
2022-03-29 17:15:04 -0300

    Ejercicio corregido

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        int resultado;
-        resultado = suma(15, 30, 25);
+       suma(15, 30, 25);
 
         Coche miCoche = new Coche();
+
         miCoche.sumarPuertas();
-        System.out.println(miCoche.puertas);
+        System.out.println("El numero de puertas es: "+ miCoche.puertas);
+
     }
 
-    public static int suma(int a, int b, int c) {
-        return a + b;
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+
     }
 }
 class Coche {
